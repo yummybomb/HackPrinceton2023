a595e05fa6a71f0620024955dd360428b62a99cf
Paul Niziolek <niziolek.paul@gmail.com>
2023-03-31 22:40:22 -0400

    implementing user model and user authentication with mongodb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info")
