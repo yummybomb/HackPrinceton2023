89282d1608904ff3af1b2b510bd71d8ff54baf2e
yummybomb <nickchen12345@gmail.com>
2023-03-31 23:13:32 -0400

    added login page and css

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './assets/LoginPage.css'
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // code to handle login submission
+  }
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
