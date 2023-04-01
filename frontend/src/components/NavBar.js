59a78b1dd82383182b44ba13ffedcde69989246a
NikhilMahalingam <mahalingamnikhil@gmail.com>
2023-04-01 13:43:25 -0400

    NavBar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import '.assets/NavBar.css';
+import React from 'react';
+import logo from './assets/logo.png';
+
+const NavBar = () => {
+  return (
+    <nav>
+    <a href="index.html"><img src={logo}/></a>
+        <div className="nav-links">
+            <ul>
+                <li><a href = "/">LOGIN</a></li>
+                <li><a href = "/dashboard">DASHBOARD</a></li>
+                <li><a href = "/generate">GENERATE</a></li>
+                <li><a href = "/completions">COMPLETIONS</a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
