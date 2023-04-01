52c19867b052c9060536e0727ebc3d9d5f2a488e
yummybomb <nickchen12345@gmail.com>
2023-03-31 23:24:15 -0400

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <LoginPage/>
+    <Router>
+      <Routes>
+        <Route>
+          <Route path="/" element={<LoginPage/>} />
+        </Route>
+      </Routes>
+    </Router>  
+
   );
 }
 
