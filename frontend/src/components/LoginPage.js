f79d9312b6aebc3d24e7dfd9b62ed1e35e04b4ad
yummybomb <nickchen12345@gmail.com>
2023-04-01 22:34:37 -0400

    Got register and login to database

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,31 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, setError } from 'react';
 import './assets/LoginPage.css'
 import ImageScroll from './ImageScroll'
+import axios from 'axios';
 
 const LoginPage = () =>  {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   }
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   }
 
-  const handleSubmit = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    // code to handle login submission
+    const formData = new FormData();
+    formData.set('username', email);
+    formData.set('password', password);
+    try {
+        await axios.post('http://localhost:8000/auth/jwt/login', 
+        formData, 
+        {
+            headers: {
+                'Content-Type' : 'multipart/form-data' 
+            },
+        },
+        )
+        alert('Login successful');
+      } catch (err) {
+        setError(err.response.data.error);
+      }
+    }
+    
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:8000/auth/register', { "email" : email, "password" : password});
+      alert('User registered successfully');
+    } catch (err) {
+      setError(err.response.data.error);
+    }
   }
 
   return (
     <div>
-      <ImageScroll></ImageScroll>
-      <form onSubmit={handleSubmit}>
+      <ImageScroll/>
+      <form>
         <label>
-          Username:
-          <input type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+          Email:
+          <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} />
         </label>
         <br />
         <label>
@@ -33,10 +60,11 @@ const LoginPage = () =>  {
           <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
         </label>
         <br />
-        <button type="submit">LOGIN</button>
+        <button onClick = {handleLogin}>LOGIN</button>
+        <button onClick = {handleRegister}>REGISTER</button>
       </form>
     </div>
-  );
+  )
 }
 
 export default LoginPage;
