3dec8f877316b8340bb81bd08aa855be498141df
NikhilMahalingam <mahalingamnikhil@gmail.com>
2023-04-01 23:01:14 -0400

    Rewards page, backend fixes

---
+++ frontend/src/components/RewardsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import treeNFT from './assets/treeNFT.png';
+import './assets/RewardsPage.css';
+
+const RewardsPage = () => {
+  return (
+    <div>
+        <NavBar></NavBar>
+        <img src={treeNFT}/>
+    </div>
+  )
+}
+
+export default RewardsPage
