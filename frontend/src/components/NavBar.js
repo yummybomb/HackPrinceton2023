fbdb3e52e4ff73185fd973a40225e615c1f3fce5
yummybomb <nickchen12345@gmail.com>
2023-04-02 06:22:26 -0400

    logout made

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,11 +8,11 @@ const NavBar = () => {
     <a href="index.html"><img src={logo}/></a>
         <div className="nav-links">
             <ul>
-                <li><a href = "/">LOGIN</a></li>
                 <li><a href = "/generate">GENERATE</a></li>
                 <li><a href = "/upload">UPLOAD</a></li>
                 <li><a href = "/judge">JUDGE</a></li>
                 <li><a href = "/rewards">REWARDS</a></li>
+                <li><a href = "/">LOGOUT</a></li>
             </ul>
         </div>
     </nav>
