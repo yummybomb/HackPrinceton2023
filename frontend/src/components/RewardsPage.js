c51922dfc22b0a96b53ea5582efd72fa2b6b6c1e
NikhilMahalingam <mahalingamnikhil@gmail.com>
2023-04-02 00:27:24 -0400

    designs

--- frontend/src/components/RewardsPage.js
+++ frontend/src/components/RewardsPage.js
@@ -6,10 +6,6 @@ import NavBar from './NavBar';
 const RewardsPage = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <NavBar/>
-        <img src={treeNFT}/>
-=======
         <NavBar></NavBar>
         <div class = "nft">
             <img src={treeNFT}/>
@@ -17,7 +13,6 @@ const RewardsPage = () => {
         <div class="center">
             <button class="mint-button">Mint!</button>
         </div>
->>>>>>> c89c113d1e0f0559b76a8464e00c41457df8dbaa
     </div>
   )
 }
